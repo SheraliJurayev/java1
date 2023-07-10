3818308d163ed59259d956e4d5b378fd500d0c99
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-07-10 13:30:03 +0500

    enter JAVA OOP

---
+++ src/one/Car.java
@@ -0,0 +1,8 @@
+package one;
+
+public class Car {
+    double price = 15000  ;
+    String marca = "Cobalt" ;
+    String color = "Jack";
+    int year = 2022 ;
+}
---
+++ src/one/Color.java
@@ -0,0 +1,8 @@
+package one;
+
+class Color {
+    public static void main(String[] args){
+       Car myObj = new Car();
+        System.out.println("My car is " + myObj.marca +", " + "year " +myObj.year + " color " + myObj.color + " price " + myObj.price);
+    }
+}
